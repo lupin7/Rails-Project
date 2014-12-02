4ecd8abdd4db025b76db57f078c9dc7120282269
Marc Estrada <mestrada36@academic.rrc.ca>
2014-12-01 17:29:47 -0800

    Added the column within the Product table in the admin dashboard

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,13 +1,14 @@
 ActiveAdmin.register Product do
 
   permit_params :name, :description, :price, :stock_quantity,
-                :category, :image_filename
+                :category, :image_filename, :sale_price
 
   form :html => { :enctype => 'multipart/form-data'} do |f|
     f.inputs "Details" do
       f.input :name
       f.input :description
       f.input :price
+      f.input :sale_price
       f.input :stock_quantity
       f.input :category
       f.input :image_filename
