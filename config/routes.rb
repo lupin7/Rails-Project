50f14b6447eb6410aca83b131a102fa40834e41d
Marc Estrada <mestrada36@academic.rrc.ca>
2014-12-11 11:58:06 -0800

    Modified the views to accomodate for functionality

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
   get 'checkout' => 'storefront#checkout', as: 'checkout'
   # get 'customer_info' => 'storefront#customer_info', as: 'details'
 
-  get "paypal_express/checkout"
+  # get "paypal_express/checkout"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
